e47eab6f7fd9bba6aff71cda1c5f45b2fbb87d3b
Alex Sherman <yul.golem@gmail.com>
2019-09-28 12:40:48 +0500

    Add nomination regeneration in performance schedule items

---
+++ app/interactions/performance_schedule/regenerate_from_nomination.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+class PerformanceSchedule
+  class RegenerateFromNomination < BaseInteraction
+    attr_accessor :nomination, :nomination_id
+
+    def nomination
+      @nomination ||= Nomination.find(nomination_id)
+    end
+
+    def run
+      ranked_performance_items = PerformanceScheduleItem.rank(:priority)
+      nomination_items = ranked_performance_items.where(nomination_id: nomination.id)
+      first_nomination_rank = nomination_items.first.priority_rank
+
+      nomination_items.destroy_all
+
+      PerformanceSchedule::GenerateFromNomination.execute(nomination: nomination)
+
+      nomination_items.reload
+      nomination_items.reverse_each do |nomination_item|
+        nomination_item.update priority_position: first_nomination_rank
+      end
+    end
+
+  end
+end
